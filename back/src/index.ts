342f1f0d0bc87d70c3ef26f5930c0b703572cd01
alevidals <alejandro.vidal.sanchez16@gmail.com>
2024-04-09 16:53:25 +0200

    feat: add back packages and setup a basic express server

---
+++ back/src/index.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response } from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  }
+})
+
+app.get('/', (req: Request, res: Response) => {
+  res.send("Working!!")
+});
+
+io.on('connection', (socket) => {
+  console.log("a user connected");
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
