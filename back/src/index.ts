1d6316bf769ac8ca92e3f90b7bb277e3c8672f24
alevidals <alejandro.vidal.sanchez16@gmail.com>
2024-04-09 17:03:22 +0200

    feat: add biome to front and back

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,23 +1,23 @@
-import express, { Request, Response } from 'express';
-import http from 'http';
-import { Server } from 'socket.io';
+import express, { type Request, type Response } from "express";
+import http from "node:http";
+import { Server } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "*",
-  }
-})
-
-app.get('/', (req: Request, res: Response) => {
-  res.send("Working!!")
+  },
 });
 
-io.on('connection', (socket) => {
+app.get("/", (req: Request, res: Response) => {
+  res.send("Working!!");
+});
+
+io.on("connection", (socket) => {
   console.log("a user connected");
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
